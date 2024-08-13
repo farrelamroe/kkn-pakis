6eb8addfa7d08ef33c2399c1277f2bd057103b84
farrelamroe <farrelamroe@gmail.com>
2024-08-13 22:57:00 +0700

    change next/image to img

---
+++ src/components/button/ArrowBack.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { Text } from "@/components";
+import { useRouter } from "next/router";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
+export function ArrowBack() {
+  const router = useRouter();
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+  return (
+    <div
+      className="mt-[111px] flex flex-row items-center justify-start gap-x-1"
+      onClick={() => router.back()}
+      data-aos="fade-right"
+    >
+      <img
+        src="/arrow.png"
+        alt=""
+        width={15}
+        height={15}
+        className="h-fit max-h-[15px] w-fit max-w-[15px]"
+      />
+      <Text
+        size="xs"
+        className="underline-animation font-jakarta duration-300 hover:cursor-pointer hover:font-bold"
+      >
+        Kembali
+      </Text>
+    </div>
+  );
+}
---
+++ src/components/template/Navbar.tsx
@@ -0,0 +1,86 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { H2, dataNavbar } from "@/components";
+
+export function Navbar() {
+  const [active, setActive] = useState(false);
+  const toggle = () => {
+    setActive((prev) => !prev);
+  };
+
+  const [hidden, setHidden] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    setHidden(currentScrollPos > prevScrollPos && currentScrollPos > 50);
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  return (
+    <>
+      <div
+        className={`fixed z-[110] h-[82px] w-[100%] transform rounded-b-[8px] bg-primary text-center font-jakarta duration-[2000ms] ${hidden ? "-top-full" : "top-0"}`}
+      >
+        <div className="relative flex h-full flex-row items-center justify-between rounded-b-[8px] px-5 shadow-md">
+          <Link href="/">
+            <img
+              src="/logo-pakis-text.png"
+              width={150}
+              height={100}
+              alt=""
+              className="relative items-center"
+            />
+          </Link>
+          <div className="relative z-40 hidden flex-row items-center justify-center gap-x-5 text-xl text-secondary lg:flex lg:pr-20">
+            {dataNavbar.map(({ title, href }) => {
+              return (
+                <>
+                  <a href={href} key={title}>
+                    <p className="underline-animation duration-300 hover:cursor-pointer hover:font-bold">
+                      {title}
+                    </p>
+                  </a>
+                </>
+              );
+            })}
+          </div>
+
+          <label className="burger lg:hidden" htmlFor="burger">
+            <input type="checkbox" id="burger" onClick={toggle} />
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+      </div>
+
+      <div
+        className={`fixed top-0 z-[110] flex h-screen w-screen flex-col items-start justify-start gap-x-5 gap-y-[50px] overflow-hidden bg-primary pt-24 font-jakarta text-xl text-secondary duration-1000 lg:hidden ${
+          active ? "max-w-[75vw]" : "max-w-0"
+        } ${hidden ? "-left-full" : "-left-0"}`}
+      >
+        {dataNavbar.map(({ title, href }) => {
+          return (
+            <>
+              <a href={href} key={title}>
+                <H2 className="whitespace-nowrap pl-[50px] duration-300 hover:cursor-pointer hover:font-bold">
+                  {title}
+                </H2>
+              </a>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+}
